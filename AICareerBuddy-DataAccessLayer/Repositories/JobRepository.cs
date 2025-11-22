2c56a7cb5ae076f38a146ef9ed2babdf5ed404d0
Franjo Čotić <fcotic22@student.foi.hr>
2025-11-22 21:43:16 +0100

    Dodane izmjene

---
+++ AICareerBuddy-DataAccessLayer/Repositories/JobRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICareerBuddy_DataAccessLayer.Repositories
+{
+    public class JobRepository
+    {
+    }
+}
---
+++ Backend/AI CareerBuddy Backend/Controllers/JobsController.cs
@@ -0,0 +1,51 @@
+﻿using AICareerBuddy_BussinesLayer.Interfaces;
+using AICareerBuddy_BussinesLayer.Services;
+using AICareerBuddy_Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AI_CareerBuddy_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly ILogger<JobService> _logger;
+        private readonly IJobService JobService;
+
+        public JobsController(ILogger<JobService> logger, JobService jobService)
+        {
+            _logger = logger;
+            JobService = jobService;
+        }
+
+        [HttpGet]
+        public IEnumerable<JobListing> Get()
+        { 
+            return JobService.GetJobs(); 
+        }
+         
+
+        [HttpGet("{id}")]
+        public JobListing Get(int id)
+        {
+            return JobService.GetJob(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
